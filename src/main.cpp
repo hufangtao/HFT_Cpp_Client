624073ebe6bb7d63aa65e81f59af35435da1a15b
hufangtao <hufangta@qq.com>
2018-12-24 22:33:15 +0800

    add

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,46 @@ boost::asio::ip::tcp::resolver resolver(io_service);
 boost::asio::ip::tcp::socket sock(io_service);
 boost::array<char, 4096> buffer;
 
-void read_handler(const boost::system::error_code &ec, std::size_t bytes_transferred){
+void read_handler(const boost::system::error_code &ec, std::size_t bytes_transferred)
+{
 
-    
-    if (!ec) {
+    if (!ec)
+    {
         /* code */
-        std::cout<<std::string(buffer.data(), bytes_transferred)<<std::endl;
+        std::cout << std::string(buffer.data(), bytes_transferred) << std::endl;
         sock.async_read_some(boost::asio::buffer(buffer), read_handler);
+    }else{
+        std::cout<<ec<<std::endl;
     }
 }
 
-void resolve_handler(const boost::system::error_code &ec, boost::asio::ip::tcp::resolver::iterator it){
-    if (!ec) {
+void connect_handler(const boost::system::error_code &ec)
+{
+
+    if (!ec)
+    {
+        /* code */
+        boost::asio::write(sock, boost::asio::buffer("GET / HTTP/1.1\r\nHost: www.highscore.de\r\n\r\n"));
+        sock.async_read_some(boost::asio::buffer(buffer), read_handler);
+    }else{
+        std::cout<<ec<<std::endl;
+    }
+}
+
+void resolve_handler(const boost::system::error_code &ec, boost::asio::ip::tcp::resolver::iterator it)
+{
+    if (!ec)
+    {
         /* code */
         sock.async_connect(*it, connect_handler);
+    }else{
+        std::cout<<ec<<std::endl;
     }
 }
 
-void connect_handler(const boost::system::error_code &ec){
-    
-    if (!ec) {
-        /* code */
-        boost::asio::write(sock, boost::asio::buffer("GET / HTTP 1.1\r\nHost: highscore.de\r\n\r\n"));
-        sock.async_read_some(boost::asio::buffer(buffer), read_handler);
-    }
-    
-}
-
-int main() {
-    boost::asio::ip::tcp::resolver::query query("localhost", "80");
+int main()
+{
+    boost::asio::ip::tcp::resolver::query query("www.highscore.de", "80");
     resolver.async_resolve(query, resolve_handler);
     io_service.run();
     return 0;
